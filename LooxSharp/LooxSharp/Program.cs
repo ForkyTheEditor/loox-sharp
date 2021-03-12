9e1b25c49b4a2d6f25a949853b7007dacbb77199
Alex Forky <20324370+ForkyTheEditor@users.noreply.github.com>
2021-03-12 15:49:18 +0200

    Initial Commit

---
+++ LooxSharp/LooxSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LooxSharp
+{
+    class LooxSharp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
